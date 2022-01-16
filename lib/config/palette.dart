5a929d5d2c78dde312af37d75b7d39881279153a
baimamboukar <baimamboukar@gmail.com>
2022-01-16 22:17:04 +0100

    added ligth color

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class Palette {
   static Color primary = const Color(0xFF36D606);
   static Color secondary = const Color(0xFF298DFF);
+  static Color ligth = Colors.white;
 }
