eb3979b98c12577eb4b930bcc523b6de04d30160
baimamboukar <baimamboukar@gmail.com>
2022-01-16 22:02:08 +0100

    council box widget

---
+++ lib/widgets/councilbox.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CouncilBox extends StatelessWidget {
+  final Council council;
+  const CouncilBox({Key? key, required this.council}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
