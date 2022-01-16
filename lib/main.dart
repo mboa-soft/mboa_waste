a3402aa659aad6df4f7fa5b9dd43205c3a61acb3
baimamboukar <baimamboukar@gmail.com>
2022-01-16 23:16:55 +0100

    main modiff

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mboa_waste/firebase_options.dart';
 
 import 'screens/screens.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MboaWaste()));
 }
 
