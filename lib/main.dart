804233f8d1306609bdb6d974891562739069d2e5
baimamboukar <baimamboukar@gmail.com>
2022-03-18 15:57:01 +0100

    root widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mboa_waste/domain/auth/auth_provider.dart';
 import 'package:mboa_waste/firebase_options.dart';
 
+import 'domain/auth/bloc/auth_bloc.dart';
 import 'screens/screens.dart';
 
 Future<void> main() async {
@@ -26,6 +29,22 @@ class MboaWaste extends StatelessWidget {
             Theme.of(context).textTheme,
           ),
         ),
-        home: const Home());
+        home: BlocProvider(
+          create: (context) => AuthBloc(provider: AuthProvider()),
+          child: BlocConsumer<AuthBloc, AuthState>(
+            listener: (context, state) {
+              // TODO: implement listener
+            },
+            builder: (context, state) {
+              if (state is LoggedIn) {
+                return const Home();
+              } else if (state is LoggedOut) {
+                return const Login();
+              } else {
+                return Container();
+              }
+            },
+          ),
+        ));
   }
 }
