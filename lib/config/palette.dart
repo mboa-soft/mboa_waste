e25521c4f0852320cea75e0551a1cb6ade746942
baimamboukar <baimamboukar@gmail.com>
2022-03-20 16:41:27 +0100

    palette adding

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -4,4 +4,6 @@ class Palette {
   static Color primary = const Color(0xFF36D606);
   static Color secondary = const Color(0xFF298DFF);
   static Color ligth = Colors.white;
+  static Color dark = Colors.white54;
+  static Color grey = Colors.grey;
 }
