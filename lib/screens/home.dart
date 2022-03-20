f3bc9925b8673d0c375cfae7a2684cf165bcf792
baimamboukar <baimamboukar@gmail.com>
2022-03-20 14:35:23 +0100

    home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:mboa_waste/config/palette.dart';
 import 'package:mboa_waste/config/styles.dart';
+import 'package:mboa_waste/widgets/widgets.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,7 +17,7 @@ class Home extends StatelessWidget {
           ),
           centerTitle: true,
           elevation: 0.0,
-          backgroundColor: Colors.white30,
+          backgroundColor: Colors.white54,
           leading: Icon(LineIcons.user, color: Palette.primary),
           actions: [
             IconButton(
@@ -28,10 +29,50 @@ class Home extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Column(
-            children: const <Widget>[
-              SizedBox(height: 20.0),
-              Text(
-                  "Recycling every single solid waste, Give it a second life, put it back into the value chain")
+            children: <Widget>[
+              const SizedBox(height: 20.0),
+              const Text(
+                  "Recycling every single solid waste, Give it a second life, put it back into the value chain"),
+              const SizedBox(height: 20.0),
+              Container(
+                height: 50.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(24.0),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    icon: Padding(
+                      padding: EdgeInsets.only(left: 12.0),
+                      child: Icon(LineIcons.search),
+                    ),
+                    border: InputBorder.none,
+                    hintText: "Give it a second life",
+                    contentPadding: EdgeInsets.all(10.0),
+                    hintStyle: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              SizedBox(
+                height: 150.0,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: CityBox(),
+                    );
+                  },
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  addAutomaticKeepAlives: true,
+                  physics: const BouncingScrollPhysics(),
+                ),
+              ),
             ],
           ),
         ),
