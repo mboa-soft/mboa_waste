6e863d390888a709d1eafcdddd8da37119c4cce6
baimamboukar <baimamboukar@gmail.com>
2022-03-20 14:35:38 +0100

    city box widget

---
+++ lib/widgets/city_box.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mboa_waste/config/palette.dart';
+import 'package:mboa_waste/config/styles.dart';
+
+class CityBox extends StatelessWidget {
+  const CityBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150.0,
+      width: 150.0,
+      child: Center(
+        child: Text(
+          "Yaounde",
+          style: Styles.designWith(Palette.primary, 20, true),
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        border: Border.all(
+          color: Palette.primary,
+          width: 1.0,
+        ),
+      ),
+    );
+  }
+}
